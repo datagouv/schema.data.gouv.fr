e9b2c0bd4711497370ebdf3829469c092b6d4385
Antoine Augusti <antoine.augusti@data.gouv.fr>
2019-08-27 16:26:12 +0200

    Improve email message on error #45
    * Improve email message on error
    
    * Add doc URL
    
    * Add dots
    
    * Mention retagging / creating a new tag

--- aggregateur/notifications.py
+++ aggregateur/notifications.py
@@ -3,6 +3,8 @@ import os
 
 
 class EmailNotification(object):
+    VALIDATION_DOC_URL = "https://schema.data.gouv.fr/documentation/validation-schemas"
+
     def __init__(self, email_to, exceptions):
         super(EmailNotification, self).__init__()
         self.email_to = email_to
@@ -10,9 +12,16 @@ class EmailNotification(object):
 
     def send(self):
         errors = "\n".join(["- " + repr(e) for e in self.exceptions])
-        text = (
-            "Bonjour !\n\nDes erreurs sont survenues lors de la validation de vos schémas.\n\n%s\n\nMerci de corriger ces erreurs."
-            % errors
+        text = "\n\n".join(
+            [
+                "Bonjour,",
+                "Cet e-mail est envoyé suite à votre soumission de schéma sur schema.data.gouv.fr.",
+                "Des erreurs sont survenues lors de la validation de vos schémas :",
+                errors,
+                "Merci de corriger ces erreurs. Vous pouvez soit créer un nouveau tag soit modifier le tag existant en intégrant vos corrections. Une fois ces erreurs corrigées, vos modifications seront intégrées sans action supplémentaire de votre part.",
+                f"Pour vous aider, vous pouvez consulter la documentation détaillant les validations effectuées sur les schémas : {self.VALIDATION_DOC_URL}.",
+                "L'équipe de data.gouv.fr",
+            ]
         )
         data = {
             "Messages": [
