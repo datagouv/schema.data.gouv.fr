f45f67745c6e8c842ff0903682acf69d47883b57
Antoine Augusti <antoine.augusti@data.gouv.fr>
2019-08-27 16:24:55 +0200

    Mention retagging / creating a new tag

--- aggregateur/notifications.py
+++ aggregateur/notifications.py
@@ -18,8 +18,8 @@ class EmailNotification(object):
                 "Cet e-mail est envoyé suite à votre soumission de schéma sur schema.data.gouv.fr.",
                 "Des erreurs sont survenues lors de la validation de vos schémas :",
                 errors,
-                "Merci de corriger ces erreurs. Une fois ces erreurs corrigées, vos modifications seront intégrées sans action supplémentaire de votre part.",
-                f"Vous pouvez consulter la documentation détaillant les validations effectuées sur les schémas : {self.VALIDATION_DOC_URL}.",
+                "Merci de corriger ces erreurs. Vous pouvez soit créer un nouveau tag soit modifier le tag existant en intégrant vos corrections. Une fois ces erreurs corrigées, vos modifications seront intégrées sans action supplémentaire de votre part.",
+                f"Pour vous aider, vous pouvez consulter la documentation détaillant les validations effectuées sur les schémas : {self.VALIDATION_DOC_URL}.",
                 "L'équipe de data.gouv.fr",
             ]
         )
