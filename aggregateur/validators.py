20740bd15bdf1306a41068f904736ee0f1f399ad
Geoffrey Aldebert <geoffrey.aldebert@data.gouv.fr>
2020-12-15 18:47:33 +0100

    Geoffrey/add external links to schema (#141)
    * Update main.py
    
    * Update validators.py to add two new properties two schemas.yml
    
    * Update layout schemas.html to add external links

--- aggregateur/validators.py
+++ aggregateur/validators.py
@@ -90,6 +90,8 @@ class BaseValidator(object):
             "type": self.repo.schema_type,
             "consolidation": self.consolidation_data(slug),
             "email": self.repo.email,
+            "external_doc":self.repo.external_doc,
+            "external_tool":self.repo.external_tool,
             "version": self.repo.current_version,
             "has_changelog": self.has_changelog,
             "schemas": self.schemas_metadata(),
