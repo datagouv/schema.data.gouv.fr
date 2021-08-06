7ae00075ba2c3ba10a5cedf31878e7fa64042f7f
Geoffrey Aldebert <geoffrey.aldebert@data.gouv.fr>
2021-08-06 16:01:21 +0200

    Rm node_modules

--- site/node_modules/@gouvfr/dsfr/src/legacy/scripts/index.js
+++
@@ -1,3 +0,0 @@
-import { ContentMediaFixLegacy } from './content-media-fix-legacy/content_media_fix_legacy';
-
-export { ContentMediaFixLegacy };
--- site/node_modules/eslint-config-vuepress/lib/babel.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  extends: [require.resolve('./index.js')],
-
-  parserOptions: {
-    parser: '@babel/eslint-parser',
-  },
-}
